ba2a3ab64637afe343072d30fddf44a4476d4697
danwt <30197399+danwt@users.noreply.github.com>
2025-04-07 16:42:14 +0100

    try to strip out instruction but not sure it will work

--- rust/sealevel/programs/hyperlane-sealevel-token-collateral-memo/src/instruction.rs
+++ rust/sealevel/programs/hyperlane-sealevel-token-collateral-memo/src/instruction.rs
@@ -2,21 +2,7 @@
 
 use account_utils::{DiscriminatorData, PROGRAM_INSTRUCTION_DISCRIMINATOR};
 use borsh::{BorshDeserialize, BorshSerialize};
-use hyperlane_sealevel_token_lib::instruction::{
-    init_instruction as lib_init_instruction, Init, TransferRemote,
-};
-
-use hyperlane_sealevel_token_collateral::{
-    hyperlane_token_ata_payer_pda_seeds, hyperlane_token_escrow_pda_seeds,
-};
-
-use solana_program::{
-    instruction::{AccountMeta, Instruction as SolanaInstruction},
-    program_error::ProgramError,
-    pubkey::Pubkey,
-    rent::Rent,
-    sysvar::SysvarId,
-};
+use hyperlane_sealevel_token_lib::instruction::TransferRemote;
 
 /// Instruction data for transferring `amount_or_id` token to `recipient`
 /// on `destination` domain, including a memo.
@@ -37,38 +23,4 @@ pub enum Instruction {
 
 impl DiscriminatorData for Instruction {
     const DISCRIMINATOR: [u8; Self::DISCRIMINATOR_LENGTH] = PROGRAM_INSTRUCTION_DISCRIMINATOR;
-}
-
-/// Gets an instruction to initialize the program.
-pub fn init_instruction(
-    program_id: Pubkey,
-    payer: Pubkey,
-    init: Init,
-    spl_program: Pubkey,
-    mint: Pubkey,
-) -> Result<SolanaInstruction, ProgramError> {
-    let mut instruction = lib_init_instruction(program_id, payer, init)?;
-
-    // Add additional account metas:
-    // 0. `[executable]` The SPL token program for the mint, i.e. either SPL token program or the 2022 version.
-    // 1. `[]` The mint.
-    // 2. `[executable]` The Rent sysvar program.
-    // 3. `[writable]` The escrow PDA account.
-    // 4. `[writable]` The ATA payer PDA account.
-
-    let (escrow_key, _escrow_bump) =
-        Pubkey::find_program_address(hyperlane_token_escrow_pda_seeds!(), &program_id);
-
-    let (ata_payer_key, _ata_payer_bump) =
-        Pubkey::find_program_address(hyperlane_token_ata_payer_pda_seeds!(), &program_id);
-
-    instruction.accounts.append(&mut vec![
-        AccountMeta::new_readonly(spl_program, false),
-        AccountMeta::new_readonly(mint, false),
-        AccountMeta::new_readonly(Rent::id(), false),
-        AccountMeta::new(escrow_key, false),
-        AccountMeta::new(ata_payer_key, false),
-    ]);
-
-    Ok(instruction)
 }
