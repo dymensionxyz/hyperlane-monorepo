16401c7a8cc92e75fe008224034ac52fcef50ba6
danwt <30197399+danwt@users.noreply.github.com>
2025-04-14 17:30:03 +0100

    Revert "Revert "Revert "fix: corect synthetic token types (#5865)"""
    This reverts commit f273a7f8106aa341f273d66d9b05c67aa08c3eb2.

--- typescript/sdk/src/token/types.ts
+++ typescript/sdk/src/token/types.ts
@@ -103,7 +103,7 @@ export const SyntheticTokenConfigSchema = TokenMetadataSchema.partial().extend({
   type: z.enum([TokenType.synthetic, TokenType.syntheticUri]),
   initialSupply: z.string().or(z.number()).optional(),
 });
-export type SyntheticTokenConfig = z.infer<typeof SyntheticTokenConfigSchema>;
+export type SyntheticTokenConfig = z.infer<typeof CollateralTokenConfigSchema>;
 export const isSyntheticTokenConfig = isCompliant(SyntheticTokenConfigSchema);
 
 export const SyntheticRebaseTokenConfigSchema =
@@ -112,7 +112,7 @@ export const SyntheticRebaseTokenConfigSchema =
     collateralChainName: z.string(),
   });
 export type SyntheticRebaseTokenConfig = z.infer<
-  typeof SyntheticRebaseTokenConfigSchema
+  typeof CollateralTokenConfigSchema
 >;
 export const isSyntheticRebaseTokenConfig = isCompliant(
   SyntheticRebaseTokenConfigSchema,
