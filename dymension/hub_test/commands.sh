bafe99282877803b8a369e593ae54235363296c9
danwt <30197399+danwt@users.noreply.github.com>
2025-04-15 15:24:16 +0100

    adds a yarn project which is used for the scripting

--- dymension/hub_test/commands.sh
+++ dymension/hub_test/commands.sh
@@ -1,4 +1,9 @@
 
+# scratch
+
+https://github.com/hyperlane-xyz/hyperlane-monorepo/tree/main/typescript/cosmos-sdk
 
 ##################################################
 # STEP: Chain start and setup
+
+anvil --port 8545 --chain-id 31337 --block-time 1 # make sure rollapp-evm not listening on same port
