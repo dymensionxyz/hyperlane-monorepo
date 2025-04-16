ee1360819dacaf2610adcfb2162d0dca4abc5d3f
danwt <30197399+danwt@users.noreply.github.com>
2025-04-16 14:59:01 +0100

    make a manual script

--- dymension/hub_test/commands.sh
+++ dymension/hub_test/commands.sh
@@ -3,8 +3,52 @@
 
 https://github.com/hyperlane-xyz/hyperlane-monorepo/tree/main/typescript/cosmos-sdk
 
-##################################################
+##############################################################################################3
 # STEP: Chain start and setup
+
+################
+# HUB: 
+
+BASE_PATH="/Users/danwt/Documents/dym/d-dymension/scripts/hyperlane_test"
+source $BASE_PATH/env.sh
+
+cd dymension/
+bash scripts/setup_local.sh
+dymd start --log_level=debug
+
+HUB_DOMAIN=31338
+ETH_DOMAIN=31337
+
+# create noop ism
+hub tx hyperlane ism create-noop "${HUB_FLAGS[@]}"
+ISM=$(curl -s http://localhost:1318/hyperlane/v1/isms | jq '.isms.[0].id' -r); echo $ISM;
+
+# create mailbox
+# ism, local domain
+hub tx hyperlane mailbox create  $ISM $HUB_DOMAIN "${HUB_FLAGS[@]}"
+MAILBOX=$(curl -s http://localhost:1318/hyperlane/v1/mailboxes   | jq '.mailboxes.[0].id' -r); echo $MAILBOX;
+
+# create noop hook
+hub tx hyperlane hooks noop create "${HUB_FLAGS[@]}"
+NOOP_HOOK=$(curl -s http://localhost:1318/hyperlane/v1/noop_hooks | jq '.noop_hooks.[0].id' -r); echo $NOOP_HOOK;
+
+# TODO: IGP needed? Gas config?!! (don't think so, for this test)
+
+# update mailbox
+# mailbox, default hook (e.g. IGP), required hook (e.g. merkle tree)
+hub tx hyperlane mailbox set $MAILBOX --default-hook $NOOP_HOOK --required-hook $NOOP_HOOK "${HUB_FLAGS[@]}"
+
+DENOM = "adym"
+hub tx hyperlane-transfer dym-create-collateral-token $MAILBOX $DENOM "${HUB_FLAGS[@]}"
+TOKEN_ID=$(curl -s http://localhost:1318/hyperlane/v1/tokens | jq '.tokens.[0].id' -r); echo $TOKEN_ID
+
+# setup the router
+# TODO: require eth token contract ``
+hub tx hyperlane-transfer enroll-remote-router $TOKEN_ID $ETH_DOMAIN $ETH_TOKEN_CONTRACT 0 "${HUB_FLAGS[@]}"
+curl -s http://localhost:1318/hyperlane/v1/tokens/$TOKEN_ID/remote_routers # check
+
+################
+# EVM: 
 
 anvil --port 8545 --chain-id 31337 --block-time 1 # make sure rollapp-evm not listening on same port
 
