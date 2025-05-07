738ceb2028a479591e0c897f0df84109d344cf83
danwt <30197399+danwt@users.noreply.github.com>
2025-05-07 17:54:31 +0100

    backup the working dymension <-> ethereum test

---
+++ dymension/dymension_test_backup/env.sh
@@ -0,0 +1,5 @@
+export HYP_KEY="0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+export HYP_ADDR="0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+export HUB_DOMAIN=1260813472 
+export ETH_DOMAIN=31337
+export DENOM="adym"
