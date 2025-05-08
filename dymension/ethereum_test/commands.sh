a4a8aaafca880f379b581454b4595495de2dfc45
Daniel T <30197399+danwt@users.noreply.github.com>
2025-05-08 15:31:36 +0100

    demonstrate what duc will e2e test (#4)
    * work on a script for eth collateral -> hub synthetic
    
    * cp
    
    * chore: forge init
    
    * forge install: forge-std
    
    v1.9.7
    
    * try to make collateral erc20 work
    
    * deploy works, now need to test it
    
    * confirms that it works collateral -> synth on hub
    
    * bump foo_test
    
    * bump foo test readme

--- dymension/ethereum_test/commands.sh
+++ dymension/ethereum_test/commands.sh
@@ -18,6 +18,8 @@ trash ~/.hyperlane; trash ~/.dymension
 ################
 # ENV: 
 
+source $HUB_BASE_PATH/env.sh
+
 export HYP_KEY="0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
 export HYP_ADDR="0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
 export HYP_ADDR_ZEROS="0x000000000000000000000000f39Fd6e51aad88F6F4ce6aB8827279cffFb92266" # this is zero padded regular address
@@ -25,8 +27,6 @@ export RELAYER_ADDR="dym15428vq2uzwhm3taey9sr9x5vm6tk78ewtfeeth" # relayer deriv
 export HUB_DOMAIN=1260813472 
 export ETH_DOMAIN=31337
 export DENOM="adym"
-
-source $HUB_BASE_PATH/env.sh
 
 ################
 # START NODES: 
