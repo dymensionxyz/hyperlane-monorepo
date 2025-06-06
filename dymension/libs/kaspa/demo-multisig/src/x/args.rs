16d26dc4e56e6bdadb0905b99710e18511610919
danwt <30197399+danwt@users.noreply.github.com>
2025-06-06 16:39:01 +0100

    fmt

--- dymension/libs/kaspa/demo-multisig/src/x/args.rs
+++ dymension/libs/kaspa/demo-multisig/src/x/args.rs
@@ -1,36 +1,29 @@
-
 use kaspa_addresses::{Address, Prefix, Version};
+use kaspa_bip32::{DerivationPath, ExtendedPublicKey};
 use kaspa_consensus_core::{
     hashing::sighash::{SigHashReusedValuesUnsync, calc_schnorr_signature_hash},
     network::{NetworkId, NetworkType},
     tx::{TransactionId, TransactionOutpoint, UtxoEntry},
 };
-use kaspa_txscript::{
-    extract_script_pub_key_address, multisig_redeem_script, opcodes::codes::OpData65,
-    pay_to_script_hash_script, script_builder::ScriptBuilder,
-};
-use kaspa_wallet_pskt::prelude::{
-    Combiner, Creator, Extractor, Finalizer, Inner, InputBuilder, OutputBuilder, PSKT, SignInputOk,
-    Signature, Signer, Updater,
-};
-// use kaspa_wallet::Wallet;
 use kaspa_core::{info, kaspad_env::version, time::unix_now, warn};
 use kaspa_grpc_client::{ClientPool, GrpcClient};
 use kaspa_notify::subscription::context::SubscriptionContext;
 use kaspa_rpc_core::{RpcUtxoEntry, api::rpc::RpcApi, notify::mode::NotificationMode};
 use kaspa_txscript::pay_to_address_script;
+use kaspa_txscript::{
+    extract_script_pub_key_address, multisig_redeem_script, opcodes::codes::OpData65,
+    pay_to_script_hash_script, script_builder::ScriptBuilder,
+};
 use kaspa_wallet_core::error::Error;
 use kaspa_wallet_core::rpc::DynRpcApi;
 use kaspa_wallet_core::storage::{IdT, PrvKeyDataInfo};
 use kaspa_wallet_core::wallet::Wallet;
-// use kaspa_wallet;
 use kaspa_wallet_core::{account::Account, api::PingRequest, api::WalletApi};
 use kaspa_wallet_keys::secret::Secret;
-// use parking_lot::Mutex;
-// use rand::RngCore;
-// use rayon::prelude::*;
-// use bip39::{Language, Mnemonic, Seed};
-use kaspa_bip32::{DerivationPath, ExtendedPublicKey};
+use kaspa_wallet_pskt::prelude::{
+    Combiner, Creator, Extractor, Finalizer, Inner, InputBuilder, OutputBuilder, PSKT, SignInputOk,
+    Signature, Signer, Updater,
+};
 
 use clap::{Arg, Command};
 
