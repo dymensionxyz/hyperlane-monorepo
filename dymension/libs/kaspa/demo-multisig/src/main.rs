fc528a0e226ffb403881904741f80f2949f17f7b
danwt <30197399+danwt@users.noreply.github.com>
2025-06-06 16:55:59 +0100

    remove unused imports

--- dymension/libs/kaspa/demo-multisig/src/main.rs
+++ dymension/libs/kaspa/demo-multisig/src/main.rs
@@ -4,45 +4,25 @@ mod x;
 use x::args::Args;
 
 use kaspa_addresses::{Address, Prefix, Version};
-use kaspa_consensus_core::{
-    hashing::sighash::{SigHashReusedValuesUnsync, calc_schnorr_signature_hash},
-    network::{NetworkId, NetworkType},
-    tx::{TransactionId, TransactionOutpoint, UtxoEntry},
-};
-use kaspa_txscript::{
-    extract_script_pub_key_address, multisig_redeem_script, opcodes::codes::OpData65,
-    pay_to_script_hash_script, script_builder::ScriptBuilder,
-};
-use kaspa_wallet_pskt::prelude::{
-    Combiner, Creator, Extractor, Finalizer, Inner, InputBuilder, OutputBuilder, PSKT, SignInputOk,
-    Signature, Signer, Updater,
-};
+use kaspa_consensus_core::
+    network::{NetworkId, NetworkType}
+;
 // use kaspa_wallet::Wallet;
-use kaspa_core::{info, kaspad_env::version, time::unix_now, warn};
-use kaspa_grpc_client::{ClientPool, GrpcClient};
+use kaspa_core::info;
+use kaspa_grpc_client::GrpcClient;
 use kaspa_notify::subscription::context::SubscriptionContext;
-use kaspa_rpc_core::{RpcUtxoEntry, api::rpc::RpcApi, notify::mode::NotificationMode};
-use kaspa_txscript::pay_to_address_script;
+use kaspa_rpc_core::notify::mode::NotificationMode;
 use kaspa_wallet_core::error::Error;
-use kaspa_wallet_core::rpc::DynRpcApi;
-use kaspa_wallet_core::storage::{IdT, PrvKeyDataInfo};
 use kaspa_wallet_core::wallet::Wallet;
 // use kaspa_wallet;
-use kaspa_wallet_core::{account::Account, api::PingRequest, api::WalletApi};
+use kaspa_wallet_core::api::WalletApi;
 use kaspa_wallet_keys::secret::Secret;
-// use parking_lot::Mutex;
-// use rand::RngCore;
-// use rayon::prelude::*;
-// use bip39::{Language, Mnemonic, Seed};
-use kaspa_bip32::{DerivationPath, ExtendedPublicKey};
 
-use clap::{Arg, Command};
 
 use std::sync::Arc;
 
-use kaspa_wrpc_client::{KaspaRpcClient, Resolver, WrpcEncoding};
+use kaspa_wrpc_client::Resolver;
 use secp256k1::{Keypair, rand::thread_rng};
-use std::{iter, str::FromStr};
 
 const URL: &str = "https://api-tn10.kaspa.org";
 const NETWORK: NetworkType = NetworkType::Testnet;
