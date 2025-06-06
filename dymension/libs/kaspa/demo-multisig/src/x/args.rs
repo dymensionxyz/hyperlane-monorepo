2b4df4539e660193b461a11ef4c1f44c3f706562
danwt <30197399+danwt@users.noreply.github.com>
2025-06-06 16:40:28 +0100

    remove unused improts

--- dymension/libs/kaspa/demo-multisig/src/x/args.rs
+++ dymension/libs/kaspa/demo-multisig/src/x/args.rs
@@ -1,37 +1,8 @@
-use kaspa_addresses::{Address, Prefix, Version};
-use kaspa_bip32::{DerivationPath, ExtendedPublicKey};
-use kaspa_consensus_core::{
-    hashing::sighash::{SigHashReusedValuesUnsync, calc_schnorr_signature_hash},
-    network::{NetworkId, NetworkType},
-    tx::{TransactionId, TransactionOutpoint, UtxoEntry},
-};
-use kaspa_core::{info, kaspad_env::version, time::unix_now, warn};
-use kaspa_grpc_client::{ClientPool, GrpcClient};
-use kaspa_notify::subscription::context::SubscriptionContext;
-use kaspa_rpc_core::{RpcUtxoEntry, api::rpc::RpcApi, notify::mode::NotificationMode};
-use kaspa_txscript::pay_to_address_script;
-use kaspa_txscript::{
-    extract_script_pub_key_address, multisig_redeem_script, opcodes::codes::OpData65,
-    pay_to_script_hash_script, script_builder::ScriptBuilder,
-};
-use kaspa_wallet_core::error::Error;
-use kaspa_wallet_core::rpc::DynRpcApi;
-use kaspa_wallet_core::storage::{IdT, PrvKeyDataInfo};
-use kaspa_wallet_core::wallet::Wallet;
-use kaspa_wallet_core::{account::Account, api::PingRequest, api::WalletApi};
-use kaspa_wallet_keys::secret::Secret;
-use kaspa_wallet_pskt::prelude::{
-    Combiner, Creator, Extractor, Finalizer, Inner, InputBuilder, OutputBuilder, PSKT, SignInputOk,
-    Signature, Signer, Updater,
-};
+use kaspa_core::kaspad_env::version;
 
 use clap::{Arg, Command};
 
-use std::sync::Arc;
 
-use kaspa_wrpc_client::{KaspaRpcClient, Resolver, WrpcEncoding};
-use secp256k1::{Keypair, rand::thread_rng};
-use std::{iter, str::FromStr};
 
 pub fn cli() -> Command {
     Command::new("rothschild")
