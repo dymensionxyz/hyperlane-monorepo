6764530cc6a25646f44256c10d5c9fc4627d4bd3
danwt <30197399+danwt@users.noreply.github.com>
2025-06-24 17:42:41 +0100

    progress checkpoint: code might be broken

--- rust/main/chains/dymension-kaspa/src/providers/easy.rs
+++ rust/main/chains/dymension-kaspa/src/providers/easy.rs
@@ -27,9 +27,49 @@ use crate::ConnectionConf;
 use eyre::Result;
 
 use hyperlane_cosmos_native::Signer as HyperlaneSigner;
+
+use core::deposit::*;
+use core::escrow::*;
+use core::util::*;
+use core::wallet::*;
+use relayer::withdraw::*;
+use validator::withdraw::*;
+use x::args::Args;
+use x::consts::*;
+
+use std::sync::Arc;
+
+use kaspa_addresses::Address;
+use kaspa_consensus_core::{
+    constants::TX_VERSION,
+    sign::sign,
+    subnets::SUBNETWORK_ID_NATIVE,
+    tx::{
+        MutableTransaction, ScriptPublicKey, Transaction, TransactionInput, TransactionOutpoint,
+        TransactionOutput, UtxoEntry,
+    },
+};
+use kaspa_core::info;
+use kaspa_grpc_client::GrpcClient;
+use kaspa_wallet_core::api::{AccountsSendRequest, WalletApi};
+use kaspa_wallet_core::error::Error;
+use kaspa_wallet_core::tx::Fees;
+
+use kaspa_wallet_core::prelude::*;
+use kaspa_wallet_pskt::prelude::*; // Import the prelude for easy access to traits/structs
+
+use kaspa_txscript::{
+    extract_script_pub_key_address, multisig_redeem_script, pay_to_address_script,
+    pay_to_script_hash_script,
+};
+
+use secp256k1::{rand::thread_rng, Keypair};
+
+use kaspa_rpc_core::api::rpc::RpcApi;
+use workflow_core::abortable::Abortable;
 struct EasyKaspaWallet {
-    // wallet: Wallet,
-    // domain: HyperlaneDomain,
+    wallet: Wallet,
+    domain: HyperlaneDomain,
 }
 
 struct EasyKaspaWalletArgs {
@@ -37,10 +77,15 @@ struct EasyKaspaWalletArgs {
     wallet_secret: String,
     // network_id: NetworkId,
     rpc_url: String, // .e.g localhost:16210
+    domain: HyperlaneDomain,
 }
 
 impl EasyKaspaWallet {
-    pub async fn new() -> Self {
+    pub async fn try_new(args: EasyKaspaWalletArgs) -> Result<Self> {
+        let s = Secret::from(args.wallet_secret);
+        let w = get_wallet(&s, NETWORK_ID, args.rpc_url)
+            .await
+            .wrap_err("Failed to get wallet")?;
         Self {}
     }
 
@@ -48,19 +93,19 @@ impl EasyKaspaWallet {
         todo!()
     }
 
-   pub fn network_id(&self) -> NetworkId {
-    todo!()
-   }
+    pub fn network_id(&self) -> NetworkId {
+        todo!()
+    }
 
-   pub fn address_prefix(&self) -> Prefix {
-    todo!()
+    pub fn address_prefix(&self) -> Prefix {
+        todo!()
+    }
 
-   }
+    pub fn address_version(&self) -> Version {
+        return Version::PubKey;
+    }
 
-   pub fn address_version(&self) -> Version {
-    return Version::PubKey;
-   }
-
-    
-    
+    pub fn rpc_url(&self) -> Arc<DynRpcApi> {
+        self.wallet.rpc_api()
+    }
 }
