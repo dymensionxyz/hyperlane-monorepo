ceab10dfefa6a22bd58c49c0e506e7c11196c501
danwt <30197399+danwt@users.noreply.github.com>
2025-06-24 15:22:02 +0100

    progress checkpoint: code might be broken

--- dymension/libs/kaspa/lib/validator/src/deposit.rs
+++ dymension/libs/kaspa/lib/validator/src/deposit.rs
@@ -1,9 +1,13 @@
-use core::{deposit::DepositFXG, confirmation::ConfirmationFXG};
+use core::{deposit::DepositFXG, confirmation::ConfirmationFXG, withdraw::WithdrawFXG};
 
 pub fn validate_deposits(fxg: &DepositFXG) -> bool {
     true
 }
 
 pub fn validate_confirmed_withdrawals(fxg: &ConfirmationFXG) -> bool {
+    true
+}
+
+pub fn validate_withdrawals(fxg: &WithdrawFXG) -> bool {
     true
 }
