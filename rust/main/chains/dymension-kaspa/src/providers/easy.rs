2c4536172185b7463c99ab7f3b66ac2b16f26cb9
danwt <30197399+danwt@users.noreply.github.com>
2025-06-25 10:49:25 +0100

    progress checkpoint: code might be broken

--- rust/main/chains/dymension-kaspa/src/providers/easy.rs
+++ rust/main/chains/dymension-kaspa/src/providers/easy.rs
@@ -13,8 +13,8 @@ use dym_kas_core::withdraw::WithdrawFXG;
 use dym_kas_relayer::withdraw_construction::on_new_withdrawals;
 use hyperlane_core::{
     rpc_clients::FallbackProvider, BlockInfo, ChainInfo, ChainResult, ContractLocator,
-    HyperlaneChain, HyperlaneDomain, HyperlaneMessage, HyperlaneProvider, HyperlaneProviderError, KnownHyperlaneDomain,
-    TxnInfo, H256, H512, U256,
+    HyperlaneChain, HyperlaneDomain, HyperlaneMessage, HyperlaneProvider, HyperlaneProviderError,
+    KnownHyperlaneDomain, TxnInfo, H256, H512, U256,
 };
 use hyperlane_metric::prometheus_metric::PrometheusClientMetrics;
 use kaspa_consensus_core::tx::Transaction;
@@ -69,7 +69,7 @@ use kaspa_rpc_core::api::rpc::RpcApi;
 use workflow_core::abortable::Abortable;
 struct EasyKaspaWallet {
     wallet: Wallet,
-    domain: HyperlaneDomain,
+    network_info: NetworkInfo,
 }
 
 struct EasyKaspaWalletArgs {
@@ -83,26 +83,30 @@ struct EasyKaspaWalletArgs {
 impl EasyKaspaWallet {
     pub async fn try_new(args: EasyKaspaWalletArgs) -> Result<Self> {
         let s = Secret::from(args.wallet_secret);
-        let w = get_wallet(&s, NETWORK_ID, args.rpc_url)
+        let info = NetworkInfo::new(args.domain, args.rpc_url);
+        let w = get_wallet(&s, info.network_id, info.rpc_url)
             .await
             .wrap_err("Failed to get wallet")?;
-        Self {}
+        Ok(Self {
+            wallet: w,
+            network_info: NetworkInfo::new(args.domain, args.rpc_url),
+        })
     }
 
     pub fn network(&self) -> NetworkType {
-        todo!()
+        self.network_info.network_type
     }
 
     pub fn network_id(&self) -> NetworkId {
-        todo!()
+        self.network_info.network_id
     }
 
     pub fn address_prefix(&self) -> Prefix {
-        todo!()
+        self.network_info.address_prefix
     }
 
     pub fn address_version(&self) -> Version {
-        return Version::PubKey;
+        self.network_info.address_version
     }
 
     pub fn rpc_url(&self) -> Arc<DynRpcApi> {
@@ -111,25 +115,26 @@ impl EasyKaspaWallet {
 }
 
 struct NetworkInfo {
-    network_id: NetworkId,
-    network_type: NetworkType,
-    address_prefix: Prefix,
-    address_version: Version,
-    rpc_url: String,
-    domain: HyperlaneDomain,
+    pub network_id: NetworkId,
+    pub network_type: NetworkType,
+    pub address_prefix: Prefix,
+    pub address_version: Version,
+    pub rpc_url: String,
+    pub domain: HyperlaneDomain,
 }
 
 impl NetworkInfo {
-    pub fn new(domain: HyperlaneDomain) -> Self {
+    pub fn new(domain: HyperlaneDomain, rpc_url: String) -> Self {
         match domain {
-            KnownHyperlaneDomain::Kaspa => {
-                Self {
-                    network_id: NetworkId::Kaspa,
-                    network_type: NetworkType::Kaspa,
-                    address_prefix: Prefix::Kaspa,
-                    address_version: Version::PubKey,
-                    rpc_url: "localhost:16210".to_string(),
-                }
+            HyperlaneDomain::Known(KnownHyperlaneDomain::KaspaTest10) => Self {
+                network_id: NetworkId::with_suffix(NetworkType::Testnet, 10),
+                network_type: NetworkType::Testnet,
+                address_prefix: Prefix::Testnet,
+                address_version: Version::PubKey,
+                rpc_url,
+                domain,
+            },
+            _ => todo!("only tn10 supported"),
         }
         // TODO: finish
     }
