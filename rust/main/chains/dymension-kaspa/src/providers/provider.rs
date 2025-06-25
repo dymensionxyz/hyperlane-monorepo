740e85387936637499682ee655c206a30987a374
danwt <30197399+danwt@users.noreply.github.com>
2025-06-25 12:15:21 +0100

    progress checkpoint: code might be broken

--- rust/main/chains/dymension-kaspa/src/providers/provider.rs
+++ rust/main/chains/dymension-kaspa/src/providers/provider.rs
@@ -98,7 +98,7 @@ impl KaspaProvider {
     }
 
     fn escrow(&self) -> EscrowPublic {
-        todo!()
+        EscrowPublic::from_strs(self.conf.validator_pks.clone(), self.easy_wallet.address_prefix(), self.conf.multisig_threshold_kaspa as u8)
     }
 }
 
