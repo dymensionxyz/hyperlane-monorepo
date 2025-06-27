e51fab81bbe51a17524fff7d2b37af1f0c8cc836
danwt <30197399+danwt@users.noreply.github.com>
2025-06-27 10:24:04 +0100

    progress checkpoint: code might be broken

--- dymension/libs/kaspa/demo/validator/src/main.rs
+++ dymension/libs/kaspa/demo/validator/src/main.rs
@@ -1,1 +1,8 @@
-fn main() {}
+use core::escrow::Escrow;
+use core::KaspaSecpKeypair;
+use secp256k1::{rand::thread_rng, Keypair, PublicKey};
+
+fn main() {
+    let kp = KaspaSecpKeypair::new(secp256k1::SECP256K1, &mut thread_rng());
+    println!("kp: {:?}", kp);
+}
