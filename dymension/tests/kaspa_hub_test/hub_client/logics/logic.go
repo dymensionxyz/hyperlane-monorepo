2af67947d89458c077d2401b9a064fc17370786e
danwt <30197399+danwt@users.noreply.github.com>
2025-06-27 12:13:42 +0100

    progress checkpoint: code might be broken

--- dymension/tests/kaspa_hub_test/hub_client/logics/logic.go
+++ dymension/tests/kaspa_hub_test/hub_client/logics/logic.go
@@ -1,4 +1,9 @@
 package logics
 
+import (
+	hypercoretypes "github.com/bcp-innovations/hyperlane-cosmos/x/core/types"
+)
+
 func (c *bridgeClient) Setup() error {
+	hypercoreC := hypercoretypes.NewQueryClient(c.hubClient.Context())
 }
