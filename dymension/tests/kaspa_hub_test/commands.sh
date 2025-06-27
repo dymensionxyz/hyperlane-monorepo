db5df447cf806e0ecf67d793f6fb7399e4487db0
danwt <30197399+danwt@users.noreply.github.com>
2025-06-27 16:30:39 +0100

    progress checkpoint: code might be broken

--- dymension/tests/kaspa_hub_test/commands.sh
+++ dymension/tests/kaspa_hub_test/commands.sh
@@ -14,32 +14,12 @@ dymd start --log_level=debug
 
 export HYP_KEY="0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
 
-hub tx hyperlane ism create-noop "${HUB_FLAGS[@]}"
-sleep 7;
-ISM=$(curl -s http://localhost:1318/hyperlane/v1/isms | jq '.isms.[0].id' -r); echo $ISM;
 
-hub tx hyperlane hooks noop create "${HUB_FLAGS[@]}"
-sleep 7;
-NOOP_HOOK=$(curl -s http://localhost:1318/hyperlane/v1/noop_hooks | jq '.noop_hooks.[0].id' -r); echo $NOOP_HOOK;
+$CLI_VALS="a,b,c"
+$CLI_THRESHOLD="1"
+CLI_REMOTE_ROUTER_ADDRESS="0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80" # arbitrary(?)
 
-hub tx hyperlane mailbox create $ISM $HUB_DOMAIN "${HUB_FLAGS[@]}"
-sleep 7;
-MAILBOX=$(curl -s http://localhost:1318/hyperlane/v1/mailboxes   | jq '.mailboxes.[0].id' -r); echo $MAILBOX;
-
-hub tx hyperlane hooks merkle create $MAILBOX "${HUB_FLAGS[@]}"
-sleep 7;
-MERKLE_HOOK=$(curl -s http://localhost:1318/hyperlane/v1/merkle_tree_hooks | jq '.merkle_tree_hooks.[0].id' -r); echo $MERKLE_HOOK;
-
-# update mailbox again. default hook (e.g. IGP), required hook (e.g. merkle tree)
-hub tx hyperlane mailbox set $MAILBOX --default-hook $NOOP_HOOK --required-hook $MERKLE_HOOK "${HUB_FLAGS[@]}"
-
-hub tx hyperlane-transfer create-synthetic-token $MAILBOX $DENOM "${HUB_FLAGS[@]}"
-sleep 7;
-TOKEN_ID=$(curl -s http://localhost:1318/hyperlane/v1/tokens | jq '.tokens.[0].id' -r); echo $TOKEN_ID
-
-hub tx hyperlane-transfer enroll-remote-router $TOKEN_ID $ETH_DOMAIN $ETH_TOKEN_CONTRACT 0 "${HUB_FLAGS[@]}" # gas = 0
-sleep 7;
-curl -s http://localhost:1318/hyperlane/v1/tokens/$TOKEN_ID/remote_routers # check
+dymd q kas setup-bridge --validators $CLI_VALS --threshold $CLI_THRESHOLD --remote-router-address $CLI_REMOTE_ROUTER_ADDRESS 
 
 # needed?
 touch ~/.hyperlane/chains/dymension/tests/addresses.yaml
