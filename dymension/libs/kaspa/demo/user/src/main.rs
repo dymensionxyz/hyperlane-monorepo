c758963259864daec7aa46c3523f6e828a98cf62
danwt <30197399+danwt@users.noreply.github.com>
2025-07-02 11:15:51 +0100

    progress checkpoint: code might be broken

--- dymension/libs/kaspa/demo/user/src/main.rs
+++ dymension/libs/kaspa/demo/user/src/main.rs
@@ -1,10 +1,19 @@
 use std::env;
-use kaspa_addresses::Address;
+use kaspa_addresses::{Address, Prefix};
 use std::str::FromStr;
+
+use hyperlane_core::H256;
+use relayer::withdraw_construction::get_recipient_address;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let addr_s = args[0];
-    let addr = kaspa_addresses::Address::from_str(&addr_s).unwrap();
+    let addr_s = args.get(1).unwrap();
+    let addr = Address::try_from(addr_s.as_str()).unwrap();
     println!("{}", addr.to_string());
+
+    let recipient = H256::random();
+
+    let prefix = Prefix::Testnet;
+    let recipient_addr = get_recipient_address(recipient, prefix);
+    println!("{}", recipient_addr.to_string());
 }
--- dymension/libs/kaspa/lib/relayer/src/withdraw_construction.rs
+++ dymension/libs/kaspa/lib/relayer/src/withdraw_construction.rs
@@ -3,12 +3,22 @@ use corelib::escrow::EscrowPublic;
 use corelib::wallet::EasyKaspaWallet;
 use corelib::withdraw::WithdrawFXG;
 use eyre::Result;
-use hyperlane_core::{Decode, HyperlaneMessage};
+use hyperlane_core::{Decode, HyperlaneMessage, H256};
 use hyperlane_cosmos_native::GrpcProvider as CosmosGrpcClient;
 use hyperlane_warp_route::TokenMessage;
 use kaspa_consensus_core::tx::TransactionOutpoint;
 use kaspa_wallet_pskt::prelude::Bundle;
 use std::io::Cursor;
+use kaspa_addresses::{Prefix, Version};
+
+pub fn get_recipient_address(recipient: H256, prefix: Prefix) -> kaspa_addresses::Address {
+    let addr = kaspa_addresses::Address::new(
+        prefix,
+        kaspa_addresses::Version::PubKey, // should always be PubKey
+        recipient.as_bytes(),
+    );
+    addr
+}
 
 /// Processes given messages and returns WithdrawFXG and the very first outpoint
 /// (the one preceding all the given transfers; it should be used during process indication).
@@ -29,11 +39,7 @@ pub async fn on_new_withdrawals(
         .filter_map(|m| {
             match TokenMessage::read_from(&mut Cursor::new(&m.body)) {
                 Ok(msg) => {
-                    let kaspa_recipient = kaspa_addresses::Address::new(
-                        relayer.network_info.address_prefix,
-                        kaspa_addresses::Version::PubKey, // should always be PubKey
-                        m.recipient.as_bytes(),
-                    );
+                    let kaspa_recipient = get_recipient_address(m.recipient, relayer.network_info.address_prefix);
 
                     Some(crate::hub_to_kaspa::WithdrawalDetails {
                         message_id: m.id(),
