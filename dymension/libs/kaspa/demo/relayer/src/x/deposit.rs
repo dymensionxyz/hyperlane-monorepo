6b0b9f68dae7296a14ed5d0b93cee5caa0b4eeaa
danwt <30197399+danwt@users.noreply.github.com>
2025-07-02 17:47:04 +0100

    progress checkpoint: code might be broken

--- dymension/libs/kaspa/demo/relayer/src/x/deposit.rs
+++ dymension/libs/kaspa/demo/relayer/src/x/deposit.rs
@@ -45,56 +45,6 @@ use api_rs::apis::kaspa_transactions_api::{
 use kaspa_rpc_core::api::rpc::RpcApi;
 use workflow_core::abortable::Abortable;
 
-pub async fn deposit(
-    w: &Arc<Wallet>,
-    secret: &Secret,
-    address: Address,
-    amt: u64,
-) -> Result<TransactionId, KaspaError> {
-    let mut hl_message = HyperlaneMessage::default();
-    let token_message = TokenMessage::new(H256::random(), U256::from(amt), vec![]);
-
-    let encoded_bytes = token_message.to_vec();
-
-    hl_message.body = encoded_bytes;
-
-    let payload = hl_message.to_vec();
-
-    deposit_impl(w, secret, address.clone(), amt, payload.clone()).await
-}
-
-pub async fn deposit_impl(
-    w: &Arc<Wallet>,
-    secret: &Secret,
-    address: Address,
-    amt: u64,
-    payload: Vec<u8>,
-) -> Result<TransactionId, KaspaError> {
-    let a = w.account()?;
-
-    let dst = PaymentDestination::from(PaymentOutput::new(address, amt));
-    let fees = Fees::from(0i64);
-    let payment_secret = None;
-    let abortable = Abortable::new();
-
-    // use account.send, because wallet.accounts_send(AccountsSendRequest{..}) is buggy
-    let (summary, _) = a
-        .send(
-            dst,
-            fees,
-            Some(payload),
-            secret.clone(),
-            payment_secret,
-            &abortable,
-            None,
-        )
-        .await?;
-
-    summary.final_transaction_id().ok_or_else(|| {
-        KaspaError::Custom("Deposit transaction failed to generate a transaction ID".to_string())
-    })
-}
-
 pub struct DemoArgs {
     pub amt: u64,
     pub escrow_address: Address,
---
+++ dymension/libs/kaspa/lib/core/src/message.rs
@@ -0,0 +1,38 @@
+pub mod api;
+pub mod confirmation;
+pub mod consts;
+pub mod deposit;
+pub mod escrow;
+pub mod payload;
+pub mod user;
+pub mod util;
+pub mod wallet;
+pub mod withdraw;
+use std::{error::Error, io::Cursor};
+
+use eyre::Result;
+use hyperlane_core::{Decode, HyperlaneMessage, RawHyperlaneMessage};
+use hyperlane_warp_route::TokenMessage;
+use kaspa_addresses::Prefix;
+use kaspa_rpc_core::RpcScriptPublicKey;
+use kaspa_txscript::extract_script_pub_key_address;
+pub use secp256k1::Keypair as KaspaSecpKeypair;
+
+pub fn parse_hyperlane_message(m: &RawHyperlaneMessage) -> Result<HyperlaneMessage, anyhow::Error> {
+    const MIN_EXPECTED_LENGTH: usize = 77;
+
+    if m.len() < MIN_EXPECTED_LENGTH {
+        return Err(anyhow::Error::msg("Value cannot be zero."));
+    }
+    let message = HyperlaneMessage::from(m);
+
+    Ok(message)
+}
+
+pub fn parse_hyperlane_metadata(m: &HyperlaneMessage) -> Result<TokenMessage, anyhow::Error> {
+    // decode token message inside  Hyperlane message
+    let mut reader = Cursor::new(m.body.as_slice());
+    let token_message = TokenMessage::read_from(&mut reader)?;
+
+    Ok(token_message)
+}
