f45673dd2fc9614c3d78a9d39e24089325c035cb
danwt <30197399+danwt@users.noreply.github.com>
2025-07-02 18:53:17 +0100

    progress checkpoint: code might be broken

--- dymension/libs/kaspa/lib/relayer/src/withdraw/mod.rs
+++ dymension/libs/kaspa/lib/relayer/src/withdraw/mod.rs
@@ -6,3 +6,5 @@ pub use hub_to_kaspa::build_withdrawal_pskt;
 pub use withdraw_construction::on_new_withdrawals;
 pub use withdraw::finalize_pskt;
 pub use withdraw::sign_pay_fee;
+pub use withdraw::build_withdrawal_tx;
+pub use withdraw::send_tx;
