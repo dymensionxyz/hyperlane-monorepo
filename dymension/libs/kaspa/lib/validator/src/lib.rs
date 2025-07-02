f534cf4817e6ef49312a7242e841e2559c090e72
danwt <30197399+danwt@users.noreply.github.com>
2025-07-02 18:41:48 +0100

    progress checkpoint: code might be broken

--- dymension/libs/kaspa/lib/validator/src/lib.rs
+++ dymension/libs/kaspa/lib/validator/src/lib.rs
@@ -2,3 +2,5 @@ pub mod confirmation;
 pub mod deposit;
 pub mod withdraw;
 pub mod signer;
+
+pub use secp256k1::Keypair as KaspaSecpKeypair;
--- dymension/libs/kaspa/lib/validator/src/withdraw.rs
+++ dymension/libs/kaspa/lib/validator/src/withdraw.rs
@@ -13,6 +13,12 @@ use hyperlane_core::HyperlaneMessage;
 use kaspa_consensus_core::hashing::sighash::{
     calc_schnorr_signature_hash, SigHashReusedValuesUnsync,
 };
+use eyre::Result;
+use corelib::withdraw::WithdrawFXG;
+
+pub async fn validate_withdrawals(fxg: &WithdrawFXG) -> Result<bool> {
+    Ok(true)
+}
 
 // Mimic a parallel multi-validator signing process
 pub fn sign_escrow_spend(e: &Escrow, pskt_unsigned: PSKT<Signer>) -> Result<PSKT<Combiner>, Error> {
