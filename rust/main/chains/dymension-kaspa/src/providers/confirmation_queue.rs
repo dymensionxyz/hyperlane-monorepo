fcd8be8c963493a1565a79aae15427b9f49f4041
danwt <30197399+danwt@users.noreply.github.com>
2025-07-03 12:04:35 +0100

    progress checkpoint: code might be broken

--- rust/main/chains/dymension-kaspa/src/providers/confirmation_queue.rs
+++ rust/main/chains/dymension-kaspa/src/providers/confirmation_queue.rs
@@ -1,26 +1,23 @@
 use dym_kas_core::confirmation::ConfirmationFXG;
-use hyperlane_core::H256;
-use hyperlane_cosmos_rs::dymensionxyz::dymension::kas::WithdrawalId;
-use std::sync::{Arc, Mutex};
+use std::sync::Mutex;
 
+#[derive(Debug, Clone)]
 pub struct ConfirmationQueue {
-    queue: Vec<ConfirmationFXG>,
-    mutex: Mutex<()>,
+    mutex: Mutex<Vec<ConfirmationFXG>>,
 }
 
 impl ConfirmationQueue {
     pub fn new() -> Self {
         Self {
-            queue: Vec::new(),
-            mutex: Mutex::new(()),
+            mutex: Mutex::new(Vec::new()),
         }
     }
-    pub fn consume(&self) -> Vec<ConfirmationFXG> {
+    pub fn consume(&mut self) -> Vec<ConfirmationFXG> {
         let mut guard = self.mutex.lock().unwrap();
         std::mem::take(&mut *guard)
     }
-    pub fn push(&self, fxg: ConfirmationFXG) {
+    pub fn push(&mut self, fxg: ConfirmationFXG) {
         let mut guard = self.mutex.lock().unwrap();
-        self.queue.push(fxg);
+        guard.push(fxg);
     }
 }
