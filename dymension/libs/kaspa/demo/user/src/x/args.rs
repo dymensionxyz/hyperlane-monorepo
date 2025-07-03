040f25ef0b63c8250c00d456ab56255298ef3a80
danwt <30197399+danwt@users.noreply.github.com>
2025-07-03 10:30:42 +0100

    progress checkpoint: code might be broken

--- dymension/libs/kaspa/demo/user/src/x/args.rs
+++ dymension/libs/kaspa/demo/user/src/x/args.rs
@@ -36,56 +36,63 @@ pub fn cli() -> Command {
                 wallet secret, network id, rpc url, payload string, escrow addr, amt
                  */
                 .arg(
-                    Arg::new("escrow-address")
+                    Arg::new(DEPOSIT_FLAG_ESCROW_ADDRESS)
                         .help("The escrow address (like kaspatest:pzlq49spp66vkjjex0w7z8708f6zteqwr6swy33fmy4za866ne90v7e6pyrfr)")
                         .required(true)
-                        .long("escrow-address")
+                        .long(DEPOSIT_FLAG_ESCROW_ADDRESS)
                 )
                 .arg(
-                    Arg::new("amount")
+                    Arg::new(DEPOSIT_FLAG_AMOUNT)
                         .help("The amount to deposit in sompi (like 100000)")
                         .required(true)
-                        .long("amount")
+                        .long(DEPOSIT_FLAG_AMOUNT)
                 )
                 .arg(
-                    Arg::new("payload")
+                    Arg::new(DEPOSIT_FLAG_PAYLOAD)
                         .help("The payload to deposit (hex without 0x prefix) (like 03000...00003e8)")
                         .required(true)
-                        .long("payload")
+                        .long(DEPOSIT_FLAG_PAYLOAD)
                 )
                 .arg(
-                    Arg::new("wrpc-url")
+                    Arg::new(DEPOSIT_FLAG_WRPC_URL)
                         .help("The rpc url (like localhost:16210)")
                         .required(true)
-                        .long("wrpc-url")
+                        .long(DEPOSIT_FLAG_WRPC_URL)
                 )
                 .arg(
-                    Arg::new("network-id")
+                    Arg::new(DEPOSIT_FLAG_NETWORK_ID)
                         .help("The kaspa network id (like testnet-10)")
                         .required(true)
-                        .long("network-id")
+                        .long(DEPOSIT_FLAG_NETWORK_ID)
                 )
                 .arg(
-                    Arg::new("wallet-secret")
+                    Arg::new(DEPOSIT_FLAG_WALLET_SECRET)
                         .help("Local kaspa wallet keychain secret (not private key)")
                         .required(true)
-                        .long("wallet-secret")
+                        .long(DEPOSIT_FLAG_WALLET_SECRET)
                 ),
         )
 }
 
+const DEPOSIT_FLAG_ESCROW_ADDRESS: &str = "escrow-address";
+const DEPOSIT_FLAG_AMOUNT: &str = "amount";
+const DEPOSIT_FLAG_PAYLOAD: &str = "payload";
+const DEPOSIT_FLAG_WRPC_URL: &str = "wrpc-url";
+const DEPOSIT_FLAG_NETWORK_ID: &str = "network-id";
+const DEPOSIT_FLAG_WALLET_SECRET: &str = "wallet-secret";
+
 impl DepositArgs {
     pub fn parse() -> Self {
         let m = cli().get_matches();
-        let network_id = m.get_one::<String>("network-id").unwrap().clone();
+        let network_id = m.get_one::<String>(DEPOSIT_FLAG_NETWORK_ID).unwrap().clone();
         let network_id = NetworkId::from_str(&network_id).unwrap();
         DepositArgs {
-            wallet_secret: m.get_one::<String>("wallet-secret").unwrap().clone(),
-            amount: m.get_one::<String>("amount").unwrap().clone(),
-            payload: m.get_one::<String>("payload").unwrap().clone(),
-            escrow_address: m.get_one::<String>("escrow-address").unwrap().clone(),
+            wallet_secret: m.get_one::<String>(DEPOSIT_FLAG_WALLET_SECRET).unwrap().clone(),
+            amount: m.get_one::<String>(DEPOSIT_FLAG_AMOUNT).unwrap().clone(),
+            payload: m.get_one::<String>(DEPOSIT_FLAG_PAYLOAD).unwrap().clone(),
+            escrow_address: m.get_one::<String>(DEPOSIT_FLAG_ESCROW_ADDRESS).unwrap().clone(),
             network_id: network_id,
-            rpc_url: m.get_one::<String>("wrpc-url").unwrap().clone(),
+            rpc_url: m.get_one::<String>(DEPOSIT_FLAG_WRPC_URL).unwrap().clone(),
         }
     }
 }
