03e75e51e8c76da803278149b0463f46519f86a0
danwt <30197399+danwt@users.noreply.github.com>
2025-07-08 12:47:53 +0100

    progress checkpoint: code might be broken

--- dymension/tests/kaspa_hub_test/commands.sh
+++ dymension/tests/kaspa_hub_test/commands.sh
@@ -17,11 +17,19 @@
 # 4. relayer 
 # 5. deposit/withdraw
 
-#### 0. Setup escrow
+###################################
+#### Step 0. START KASPA RPC NODE
+#### This is needed for WPRC queries and wallet connections. It takes a while to sync
+
+# see instructions at
+# https://github.com/dymensionxyz/hyperlane-monorepo/blob/ad21e8a6554999033b39949cb80c13c208bc3581/dymension/libs/kaspa/demo/multisig/README.md#L32
+
+###################################
+#### Step 1. Setup escrow
+#### Create hyperlane validators keys and addresses, and kaspa escrow keys and address, seed the escrow
 
 # in libs/kaspa/demo/user
 cargo run validator-with-escrow
-# THES VALUES MUST CORRESPOND WITH agent-config.json, AND the CLI commands below. Do NOT unescape json quotes
 #   "validator_ism_addr": "\"0xc09dddbd26fb6dcea996ba643e8c2685c03cad5a7\"",
 #   "validator_ism_priv_key": "c02e29cb65e55b3af3d8dee5d7a30504ed927436caf2e53e1e965cbd2639aced",
 #   "validator_escrow_secret": "\"11013bc86d1cb199a2324130c808e90ad37d07ae8f490d063b2fb9d9aa2e898f\"",
@@ -32,6 +40,7 @@ VALIDATOR_ISM_PRIV_KEY="c02e29cb65e55b3af3d8dee5d7a30504ed927436caf2e53e1e965cbd
 VALIDATOR_ESCROW_SECRET="\"11013bc86d1cb199a2324130c808e90ad37d07ae8f490d063b2fb9d9aa2e898f\""
 VALIDATOR_ESCROW_PUB_KEY="02b1c7b586c8a0387a3c844f6a5471130bb7992346d3e906642cfd5dfce8a8129d"
 ESCROW_ADDR="kaspatest:pzlq49spp66vkjjex0w7z8708f6zteqwr6swy33fmy4za866ne90v7e6pyrfr"
+# THES VALUES MUST CORRESPOND WITH agent-config.json (in this directory, REQUIRES EDITING)  Do NOT unescape json quotes
 
 # in rusty-kaspa/wallet/native
 cargo run
@@ -41,7 +50,9 @@ select
 send 1 $ESCROW_ADDR
 # seed escrow with a few kas 
 
-#### 1. Setup HUB
+###################################
+#### Step 2. Setup HUB
+#### Deploy hyperlane entities 
 
 MONODIR=/Users/danwt/Documents/dym/d-hyperlane-monorepo
 
@@ -59,12 +70,9 @@ dymd start --log_level=debug
 REMOTE_ROUTER_ADDRESS="0x0000000000000000000000000000000000000000000000000000000000000000" # no smart contracts on kaspa 
 dymd q kas setup-bridge --validators "$VALIDATOR_ISM_ADDR" --threshold 1 --remote-router-address "$REMOTE_ROUTER_ADDRESS" "${HUB_FLAGS[@]}"
 
-#### 2. START KASPA RPC NODE
-
-# start wprc node
-# https://github.com/dymensionxyz/hyperlane-monorepo/blob/ad21e8a6554999033b39949cb80c13c208bc3581/dymension/libs/kaspa/demo/multisig/README.md#L32
-
-#### 3. SETUP VALIDATOR
+###################################
+#### Step 3. SETUP VALIDATOR
+#### It will start listening for relayer requests
 
 AGENT_TMP=/Users/danwt/Documents/dym/aaa-dym-notes/all_tasks/tasks/202505_feat_kaspa/practical/e2e/tmp
 DB_RELAYER=$AGENT_TMP/dbs/hyperlane_db_relayer
@@ -88,21 +96,26 @@ RUST_BACKTRACE=1 cargo run --release --bin validator -- \
   --metrics-port 9090 \
   --log.level info 
 
-#### 4. BOOTSTRAP HUB
+###################################
+#### Step 4. BOOTSTRAP HUB
+#### Need to declare to the hub that the bridge is ready, by specifying the escrow seed outpoint
+#### We submit a gov proposal with the seed outpoint and mailbox
 
-ISM=$(dymd q hyperlane ism isms -o json | jq -r '.isms[0].id')
-MAILBOX=$(dymd q hyperlane mailboxes -o json | jq -r '.mailboxes[0].id')
-TOKEN_ID=$(dymd q warp tokens -o json | jq -r '.tokens[0].id')
-HUB_USER_ADDR=$(dymd keys show -a hub-user) #dym139mq752delxv78jvtmwxhasyrycufsvrw4aka9
+# First construct the proposal
 
-dymd q auth module-account gov -o json | jq -r '.account.value.address' # dym10d07y265gmmuvt4z0w9aw880jnsr700jgllrna
+dymd q auth module-account gov -o json | jq -r '.account.value.address' # get the authority dym10d07y265gmmuvt4z0w9aw880jnsr700jgllrna
+
+# get the kaspa seed outpoint
 
 curl -X 'GET' 'https://api-tn10.kaspa.org/addresses/kaspatest%3Apzlq49spp66vkjjex0w7z8708f6zteqwr6swy33fmy4za866ne90v7e6pyrfr/utxos' -H 'accept: application/json' # TODO: query escrow address (fix url encoding)
 OUTPOINT="5e1cf6784e7af1808674a252eb417d8fa003135190dd4147caf98d8463a7e73a"
-# need to convert outpoint to base64 when passing to hub (note, zero index does not render)
+# need to convert outpoint from hex to base64 when passing to hub (note, zero index does not render)
 echo $OUTPOINT | xxd -r -p | base64 # Xhz2eE568YCGdKJS60F9j6ADE1GQ3UFHyvmNhGOn5zo=
-# note, reverse id echo $base64 | base64 -D | xxd -p
+# note, reverse is ` echo $base64 | base64 -D | xxd -p `
 
+# query the hub entities and reference them (REQUIRES EDITING bootstrap.json)
+ISM=$(dymd q hyperlane ism isms -o json | jq -r '.isms[0].id')
+MAILBOX=$(dymd q hyperlane mailboxes -o json | jq -r '.mailboxes[0].id')
 dymd tx gov submit-proposal $MONODIR/dymension/tests/kaspa_hub_test/bootstrap.json \
   --from hub-user \
   --gas auto \
@@ -111,7 +124,7 @@ dymd tx gov submit-proposal $MONODIR/dymension/tests/kaspa_hub_test/bootstrap.js
 
 dymd tx gov vote 1 yes "${HUB_FLAGS[@]}" 
 
-#### 5. SETUP RELAYER 
+#### Step 5. SETUP RELAYER 
 
 # TODO: cleanups, kaspatest10.signer not actually used
 
@@ -129,9 +142,12 @@ dymd tx gov vote 1 yes "${HUB_FLAGS[@]}"
     --metrics-port 9091 \
     --log.level debug 
 
-#### 5. SUBMIT DEPOSITS/WITHDRAWALS
+#### Step 6. SUBMIT DEPOSITS/WITHDRAWALS
 
 # *DEPOSITS*
+
+TOKEN_ID=$(dymd q warp tokens -o json | jq -r '.tokens[0].id')
+HUB_USER_ADDR=$(dymd keys show -a hub-user) #dym139mq752delxv78jvtmwxhasyrycufsvrw4aka9
 
 DEPOSIT_AMT=100000000 # 100 million sompi = 1 TKAS
 
