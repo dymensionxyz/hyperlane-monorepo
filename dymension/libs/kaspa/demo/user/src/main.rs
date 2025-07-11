d3e054036fefc3731e9e019a654e451daed98cf6
danwt <30197399+danwt@users.noreply.github.com>
2025-07-11 12:38:32 +0100

    progress checkpoint: code might be broken

--- dymension/libs/kaspa/demo/user/src/main.rs
+++ dymension/libs/kaspa/demo/user/src/main.rs
@@ -24,9 +24,15 @@ async fn run(cli: Cli) {
             let e = x::escrow::get_escrow_address(pub_keys);
             println!("Escrow address: {}", e);
         }
-        Commands::Validator => {
-            let (v, _) = x::escrow::create_validator();
-            println!("Validator infos: {}", v.to_string());
+        Commands::Validator(args) => {
+            let mut infos = vec![];
+            for _ in 0..args.n {
+                let (v, _) = x::escrow::create_validator();
+                infos.push(v);
+            }
+            // sort required by Hyperlane Cosmos ISM creation
+            infos.sort_by(|a, b| a.validator_ism_addr.cmp(&b.validator_ism_addr));
+            println!("{}", serde_json::to_string_pretty(&infos).unwrap());
         }
         Commands::ValidatorAndEscrow => {
             let v = x::escrow::create_validator_with_escrow();
