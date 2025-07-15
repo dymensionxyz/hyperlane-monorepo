910d06090544f8ec72ca1f8b6c6989ed370e10db
danwt <30197399+danwt@users.noreply.github.com>
2025-07-15 18:24:02 +0100

    progress checkpoint: code might be broken

--- dymension/libs/kaspa/lib/core/src/finality/finality.rs
+++ dymension/libs/kaspa/lib/core/src/finality/finality.rs
@@ -1,5 +1,4 @@
-/// Refactored copy
-/// https://github.com/kaspanet/rusty-kaspa/blob/v1.0.0/wallet/core/src/storage/transaction/record.rs
+use crate::api::client::HttpClient;
 use eyre::Result;
 use kaspa_consensus_core::network::NetworkId;
 use kaspa_rpc_core::RpcHash;
@@ -31,7 +30,24 @@ pub async fn is_tx_final(
 /// is reorged.
 /// Unsuitable for doing off-chain work such as minting on a bridge.
 pub fn is_mature(daa_score_block: u64, daa_score_virtual: u64, network_id: NetworkId) -> bool {
+    //  see https://github.com/kaspanet/rusty-kaspa/blob/v1.0.0/wallet/core/src/storage/transaction/record.rs
     let params = NetworkParams::from(network_id);
     let maturity = params.user_transaction_maturity_period_daa();
     daa_score_virtual >= daa_score_block + maturity
+}
+
+/// returns true if accepted and final (reorg with low probability)
+pub async fn is_final(
+    wrpc_client: &Arc<DynRpcApi>,
+    rest_client: &HttpClient,
+    tx_id: &str,
+) -> Result<bool> {
+    let tx = rest_client.get_tx_by_id(tx_id).await?;
+    let accepting = tx
+        .accepting_block_hash
+        .ok_or(eyre::eyre!("Accepting block hash is missing"))?;
+    let accepting_blue_score = tx
+        .accepting_block_blue_score
+        .ok_or(eyre::eyre!("Accepting block blue score is missing"))?;
+    Ok(true)
 }
