ee4ffcfa6feaca717b1c5cfc5f0c02a8e5725540
danwt <30197399+danwt@users.noreply.github.com>
2025-07-24 10:35:09 +0100

    progress checkpoint: code might be broken

--- dymension/libs/kaspa/demo/user/src/sim/foo.rs
+++ dymension/libs/kaspa/demo/user/src/sim/foo.rs
@@ -1,10 +1,14 @@
+use hyperlane_cosmos_native::GrpcProvider as CosmosGrpcClient;
+use corelib::wallet::EasyKaspaWallet;
+
 pub struct TrafficSim {
-    n: u32,
+    cosmos_rpc: CosmosGrpcClient,
+    easy_wallet: EasyKaspaWallet,
 }
 
 impl TrafficSim {
-    pub fn new(n: u32) -> Self {
-        Self { n }
+    pub fn new() -> Self {
+        Self {}
     }
 
     pub async fn run(&self) -> Result<(), Box<dyn std::error::Error>> {
